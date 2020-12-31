abdac0d2d44e995a83a73cf6d8637dbc7d0ec866
user <hamza.h.hamud@gmail.com>
2020-12-31 18:10:25 +0000

    changed settings

--- backend/blog_backend/settings/production.py
+++ backend/blog_backend/settings/production.py
@@ -1,16 +1,18 @@
 
-from settings.base import *
+from .base import *
 from pathlib import Path
 import os
 
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
+
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "agile-citadel-92315.herokuapp.com"]
 
 
