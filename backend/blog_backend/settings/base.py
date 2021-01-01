9c80a546cf9286c4bb01f29f09867e699c8e49d5
user <hamza.h.hamud@gmail.com>
2021-01-01 19:23:10 +0000

    staticfiles change

--- backend/blog_backend/settings/base.py
+++ backend/blog_backend/settings/base.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build', 'static')]
-STATIC_ROOT = os.path.join(BACKEND_DIR, 'static')
+STATIC_ROOT = os.path.join(BACKEND_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
